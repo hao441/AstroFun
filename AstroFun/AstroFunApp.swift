755ebdc307a5e60194e7326802a589d94c5b7a57
Hao <har@mailbox.org>
2022-12-15 12:02:59 +1100

    Initial Commit

---
+++ AstroFun/AstroFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroFunApp.swift
+//  AstroFun
+//
+//  Created by Hao on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AstroFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
